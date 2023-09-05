9eccc2a3947de1ff1d26ca9c8531528ccd499417
Artusamak <julien@happyculture.coop>
2023-09-05 12:44:16 +0200

    Remove debug.

--- site/assets/js/camp.js
+++ site/assets/js/camp.js
@@ -1,33 +1,30 @@
-console.log('in hello');
 const $button = document.querySelector("[data-js-selector='navigation']");
-console.log($button);
 if ($button) {
   $button.addEventListener('click', function(e) {
     $content_selector = e.target.getAttribute('aria-controls');
     $content = document.getElementById($content_selector);
     if ($content) {
       expanded_state = $content.getAttribute('aria-expanded');
-      console.debug($content);
-      console.debug(expanded_state);
+      // Initiate status, we don't have default value on the first click.
       if (expanded_state == null) {
-        $content.setAttribute('aria-expanded', false);
         expanded_state = false;
         button_label = 'Afficher la navigation';
         $button.setAttribute('aria-label', button_label);
+        $content.setAttribute('aria-expanded', expanded_state);
       }
-      if (expanded_state == "true") {
-        $content.setAttribute('aria-expanded', false);
+      // Toggle status.
+      else if (expanded_state == "true") {
         button_label = 'Afficher la navigation';
         $button.setAttribute('aria-label', button_label);
         $button.classList.remove('js-close');
+        $content.setAttribute('aria-expanded', false);
       }
       else {
-        $content.setAttribute('aria-expanded', true);
         button_label = 'Masquer la navigation';
         $button.setAttribute('aria-label', button_label);
         $button.classList.add('js-close');
+        $content.setAttribute('aria-expanded', true);
       }
-      expanded_state = $content.getAttribute('aria-expanded');
     }
 
   });
