e34bbcd35fb8114ef2461878c75cd01ff2406f60
Artusamak <julien@happyculture.coop>
2023-11-24 15:54:13 +0100

    Toggle tickets links on mobile.

--- site/assets/js/camp.js
+++ site/assets/js/camp.js
@@ -1,8 +1,12 @@
 const $button = document.querySelector("[data-js-selector='navigation']");
 if ($button) {
   $button.addEventListener('click', function(e) {
+    // Menu block.
     $content_selector = e.target.getAttribute('aria-controls');
     $content = document.getElementById($content_selector);
+
+    // Tickets link block.
+    $ticket_link = document.querySelector('[data-js-selector="nav-tickets"]');
     if ($content) {
       expanded_state = $content.getAttribute('aria-expanded');
       // Initiate status, we don't have default value on the first click.
@@ -11,6 +15,9 @@ if ($button) {
         button_label = 'Afficher la navigation';
         $button.setAttribute('aria-label', button_label);
         $content.setAttribute('aria-expanded', expanded_state);
+        if ($ticket_link) {
+          $ticket_link.classList.remove('js-close');
+        }
       }
 
       // Toggle status.
@@ -19,12 +26,19 @@ if ($button) {
         $button.setAttribute('aria-label', button_label);
         $button.classList.remove('js-close');
         $content.setAttribute('aria-expanded', false);
+        if ($ticket_link) {
+          $ticket_link.classList.remove('js-close');
+        }
       }
       else {
         button_label = 'Masquer la navigation';
         $button.setAttribute('aria-label', button_label);
         $button.classList.add('js-close');
         $content.setAttribute('aria-expanded', true);
+
+        if ($ticket_link) {
+          $ticket_link.classList.add('js-close');
+        }
       }
     }
 
